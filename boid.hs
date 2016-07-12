5adbaa395c6ddeaf820c3d5261e6d60bc10058ed
OtaRyo <nwtgck@gmail.com>
2016-07-12 22:48:19 +0900

    [change] ボイドで群れの中心に行くルールを適用した

--- boid.hs
+++ boid.hs
@@ -5,12 +5,14 @@ import           FRP.Yampa
 import           Graphics.UI.GLUT
 import           System.Random
 
-data Boid = Boid{pos :: (Double, Double), vel :: (Double, Double)}
+newtype Id = Id Int deriving Eq
+data Boid = Boid{ident :: Id, pos :: (Double, Double), vel :: (Double, Double)}
 
-width = 600
-height = 400
-boidSize = 5
-numBoids = 100
+width = 700
+height = 500
+boidSize = 3
+numBoids = 1000
+maxSpeed = 10
 
 mainSF :: [Boid] -> SF () (IO ())
 mainSF boids = loopPre boids coreSF
@@ -23,13 +25,34 @@ mainSF boids = loopPre boids coreSF
 
 move :: SF [Boid] [Boid]
 move  = proc boids -> do
-  let nextBoids = forwardBoids boids
+  let rule1Boids = map (rule1 boids) boids
+  let nextBoids = forwardBoids rule1Boids
   returnA -< nextBoids
 
 forwardBoids :: [Boid] -> [Boid]
 forwardBoids [] = []
 forwardBoids (b@Boid{pos=(x,y), vel=(vx,vy)}:bs) =
-  b{pos=(x+vx, y+vy)}: forwardBoids bs
+  let speed = sqrt (vx * vx + vy * vy)
+      (newVx, newVy) = if (speed >= maxSpeed)
+        then let r = maxSpeed / speed in (vx * r, vy * r)
+        else (vx, vy)
+      newVx2 = if (x < 0 && newVx < 0 || x > width && newVx > 0) then -newVx else newVx
+      newVy2 = if (y < 0 && newVy < 0 || y > height && newVy > 0) then -newVy else newVy
+
+  in b{pos=(x+newVx2, y+newVy2)}: forwardBoids bs
+
+
+rule1 :: [Boid] -> Boid -> Boid
+rule1 boids boid@(Boid{ident=ident,pos=(x, y), vel=(vx,vy)}) =
+  let (sumX, sumY) = sumBoids boids
+      (cx, cy) = (sumX / fromIntegral (length boids - 1), sumY / fromIntegral(length boids - 1))
+  in boid{vel=(vx+ (cx-x)/50, vy+ (cy-y)/50)}
+  where
+    sumBoids :: [Boid] -> (Double, Double) -- TODO 自分以外のsumにしないといけない
+    sumBoids [] = (0, 0)
+    sumBoids (Boid{ident=ident2, pos=(x, y)}:bs) =
+      let (sx, sy) = sumBoids bs
+      in if ident==ident2 then (sx, sy) else (x+sx, y+sy)
 
 draw :: [Boid] -> IO ()
 draw boids = do
@@ -51,7 +74,7 @@ reshape size = do
 
 idle :: ReactHandle () (IO ()) -> IO ()
 idle rh = do
-  react rh (0.06, Just ())
+  react rh (0.1, Just ())
   return ()
 
 initGL :: IO ()
@@ -73,7 +96,7 @@ randomBoids n = do
   let (x,g2) = randomR (0, width) g1
   let (y,_) = randomR (0, height) g2
   rest <- randomBoids (n-1)
-  return $ (Boid (x, y) (1, 1)) : rest
+  return $ (Boid (Id n) (x, y) (0, 0)) : rest
 
 main :: IO ()
 main = do
